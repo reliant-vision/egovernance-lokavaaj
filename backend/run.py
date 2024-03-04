377fc74d314a6d9404da9dd0804529cb1f0816b4
ShivakumarReliant <shivakumar@reliantvision.com>
2024-03-04 14:56:55 +0530

    updated nginx.conf and run.py

--- backend/run.py
+++ backend/run.py
@@ -4,8 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__, template_folder='app/templates')
 
 # Allow requests from your React app's domain
-# CORS(app, resources={r"/api/*": {"origins": "http://3.111.163.245"}})
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "http://3.111.163.245"}})
 
 @app.route('/')
 def dashboard():
