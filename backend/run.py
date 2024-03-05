dcede8545332f663c28c5c2fec22d819e8662dc8
ShivakumarReliant <shivakumar@reliantvision.com>
2024-03-05 16:11:30 +0530

    Updated run.py

--- backend/run.py
+++ backend/run.py
@@ -21,3 +21,7 @@ def create_application():
 @app.route('/DepartmentLogin')
 def department_login():
     return render_template('DepartmentLogin.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
