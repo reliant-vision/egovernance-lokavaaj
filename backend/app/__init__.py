0d9e96fc3030811184af977b10643ab6bd5d85ad
ShivakumarReliant <shivakumar@reliantvision.com>
2024-03-12 02:47:27 +0530

    updated frontend and backend

--- backend/app/__init__.py
+++
@@ -1,8 +0,0 @@
-# # backend/app/__init__.py
-# from flask import Flask
-
-# app = Flask(__name__, template_folder='app/templates')
-
-# print("Flask app initialized")
-# # Import routes after creating Flask app to avoid circular import
-# from backend.app import routes
