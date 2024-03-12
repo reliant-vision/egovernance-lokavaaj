de5c9c193ebca9f74c9f6300d49b6f29c06e7a85
ShivakumarReliant <shivakumar@reliantvision.com>
2024-03-12 14:59:51 +0530

    Updated run.py and added .ebextensions folder

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
 from main import create_app
 from config import DevConfig
+import os
 
 if __name__ == '__main__':
     app=create_app(DevConfig)
-    app.run(host='0.0.0.0', debug=False, port=5000) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', debug=False, port=port)
