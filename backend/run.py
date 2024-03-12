8de30357f2727e6517130e69504a43cc86ced411
ShivakumarReliant <shivakumar@reliantvision.com>
2024-03-12 16:20:02 +0530

    updated run.py

--- backend/run.py
+++ backend/run.py
@@ -4,5 +4,5 @@ import os
 
 if __name__ == '__main__':
     app=create_app(DevConfig)
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', debug=False, port=port)
